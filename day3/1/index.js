ce6c335186c85dd7f4e15918c4045a102fe0d735
Martin Grantz <martin.grantz@ffcg.se>
2020-12-03 10:46:40 +0100

    removed console.log

--- day3/1/index.js
+++ day3/1/index.js
@@ -8,9 +8,8 @@ const world = helpers
 
 let x = 0, y = 0, trees = 0;
 const maxY = world.length - 1, maxX = world[0].length - 1;
-console.log(maxX, maxY)
+
 while (y <= maxY) {
-  console.log(x, y, world[y][x] === '#')
   if (world[y][x] === '#') trees++;
   x += 3;
   y += 1;
